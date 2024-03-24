87c38e577ceceee7704d6f4558f77c276d371957
Abdelrhman Abdelfattah <you@example.com>
2024-03-24 21:19:51 +0200

    finished news app

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:news_app/data/api/api_manager.dart';
 import 'package:news_app/data/model/category_dm.dart';
 import 'package:news_app/ui/screens/home/tabs/categories/categories_tab.dart';
 import 'package:news_app/ui/screens/home/tabs/news/news_tab.dart';
+import 'package:news_app/ui/screens/home/tabs/settings/settings_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -43,6 +44,7 @@ class _HomeScreenState extends State<HomeScreen> {
           )),
         ),
         body: currentTab,
+        drawer: buildDrawer(),
       ),
     );
   }
@@ -50,5 +52,60 @@ class _HomeScreenState extends State<HomeScreen> {
   onCategoryClick(CategoryDM categoryDM){
     currentTab = NewsTab();
     setState(() {});
+  }
+
+  buildDrawer()=>Drawer(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Center(
+                child: Text(
+                  "News app",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ))),
+        buildDrawerRow(Icons.list, "Categories", ()
+        {
+          currentTab = CategoriesTab(onCategoryClick);
+          setState(() {});
+          Navigator.pop(context);
+        }),
+        buildDrawerRow(Icons.settings, "Settings", ()
+        {
+          currentTab= const SettingsTab();
+          setState(() {});
+          Navigator.pop(context);
+        }),
+      ],
+    ),
+  );
+
+  buildDrawerRow(IconData iconData, String title, Function onClick)
+  {
+    return InkWell(
+      onTap: ()
+      {
+        onClick();
+      },
+      child: Row(
+        children: [
+          const SizedBox(width: 16,),
+          Icon(
+            iconData,
+            color: Colors.black,
+            size: 35,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
   }
 }
